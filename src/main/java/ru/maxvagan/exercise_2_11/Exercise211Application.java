264b92310688ab8dec68bba7ab0a0e4d6520c767
Maxim_Vagan <maxut@gmail.com>
2022-07-04 18:29:05 +0300

    Создан проект ДЗ 2.11

---
+++ src/main/java/ru/maxvagan/exercise_2_11/Exercise211Application.java
@@ -0,0 +1,13 @@
+package ru.maxvagan.exercise_2_11;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Exercise211Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Exercise211Application.class, args);
+    }
+
+}
